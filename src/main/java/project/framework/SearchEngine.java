2beb158502b43d4b29652941f09bc2292a1fa0e6
ferrarif1w <95565152+ferrarif1w@users.noreply.github.com>
2022-05-24 11:18:35 +0200

    update

--- src/main/java/project/framework/SearchEngine.java
+++ src/main/java/project/framework/SearchEngine.java
@@ -4,12 +4,18 @@ import java.util.Vector;
 
 public class SearchEngine {
     Vector<Service> searchResults;
+    static SearchEngine instance;
 
     SearchEngine() {
         clearSearchResults();
     }
 
     private void clearSearchResults() {searchResults = new Vector<>();}
+
+    public static SearchEngine getInstance() {
+        if (instance == null) instance = new SearchEngine();
+        else return instance;
+    }
 
     public Vector<Service> performSearch(Vector<String> countries, Vector<Provider> providers, Vector<String> types, Vector<String> statuses) {
         clearSearchResults();
