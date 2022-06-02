00bc1c3e513f18f641d21cbde4567d6d6a183c0d
ferrarif1w <95565152+ferrarif1w@users.noreply.github.com>
2022-06-02 10:10:27 +0200

    new tests

--- src/test/java/project/framework/CriteriaListFactoryTest.java
+++ src/test/java/project/framework/CriteriaListFactoryTest.java
@@ -3,6 +3,7 @@ package project.framework;
 import org.junit.jupiter.api.*;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.Vector;
 
@@ -10,17 +11,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CriteriaListFactoryTest {
-    private CriteriaListFactory testObject;
     @BeforeAll
     void setUp() throws IOException {
-        testObject = new CriteriaListFactory();
+        CriteriaListFactory testObject = new CriteriaListFactory();
     }
     @Test
-    public void evaluateCorrectDataStructorBuilding() throws IOException {
-        System.out.println("test evaluateCorrectDataStructorBuilding");
-        assertTrue(!CriteriaListFactory.getCountryList().isEmpty() &&
-                !CriteriaListFactory.getProviderList().isEmpty() &&
-                !CriteriaListFactory.getTypeList().isEmpty() &&
-                !CriteriaListFactory.getStatusList().isEmpty());
+    public void evaluateCorrectDataStructureBuilding() {
+        System.out.println("test evaluateCorrectDataStructureBuilding");
+        Map<String, String> countries = CriteriaListFactory.getCountryList();
+        Vector<Provider> providers = CriteriaListFactory.getProviderList();
+        Vector<String> types = CriteriaListFactory.getTypeList();
+        Vector<String> statuses = CriteriaListFactory.getStatusList();
+        assertTrue(!countries.isEmpty() && !providers.isEmpty() &&
+                !types.isEmpty() && !statuses.isEmpty() && !countries.containsKey(null) &&
+                !countries.containsValue(null) && !providers.contains(null) &&
+                !types.contains(null) && !statuses.contains(null));
+    }
+    @Test
+    public void evaluateCorrectCountryList() {
+        Map<String, String> countries = CriteriaListFactory.getCountryList();
+        for (String code : countries.keySet()) {
+            assertEquals(2, code.length());
+        }
+    }
+    @Test
+    public void evaluateCorrectProviderList() {
+        Vector<Provider> providers = CriteriaListFactory.getProviderList();
+        for (Provider p : providers) {
+            assertEquals(p.getClass(), Provider.class);
+        }
     }
 }
